361807a670451e49edc8193724d7aa1dcd45f3f0
ZZSZSZSZZ <13023184453@163.com>
2023-10-21 15:27:04 +0800

    修改

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from models.openapi import openapi
 
-
-url = "https://u245099-8c76-975ff0f0.westb.seetacloud.com:8443/generate"
-
 openapi = openapi()
 
-for output, tokens in openapi.get_streaming_response("Hi, how are you?", url, 0, 512):
-    print(output, end='')
+for message in openapi.test("Hi, how are you?"):
+    print(message, end='')
--- models/openapi.py
+++ models/openapi.py
@@ -3,6 +3,11 @@ import requests  # HTTP 请求库
 from typing import Iterable, List
 
 
+openapi_url = "https://u245099-8c76-975ff0f0.westb.seetacloud.com:8443/generate"
+openapi_session_id = 0
+openapi_request_output_len = 512
+
+
 class openapi:
     def get_streaming_response(self,
                                prompt: str,
@@ -14,7 +19,7 @@ class openapi:
                                sequence_end: bool = True,
                                ignore_eos: bool = False) -> Iterable[List[str]]:
         headers = {'User-Agent': 'Test Client'}
-        pload = {
+        data = {
             'prompt': prompt,
             'stream': stream,
             'session_id': session_id,
@@ -23,7 +28,7 @@ class openapi:
             'sequence_end': sequence_end,
             'ignore_eos': ignore_eos
         }
-        response = requests.post(api_url, headers=headers, json=pload, stream=stream)
+        response = requests.post(api_url, headers=headers, json=data, stream=stream)
 
         for chunk in response.iter_lines(chunk_size=8192, decode_unicode=False, delimiter=b'\n'):
             if chunk:
@@ -31,3 +36,7 @@ class openapi:
                 output = data['text']
                 tokens = data['tokens']
                 yield output, tokens
+
+    def test(self, prompt: str):
+        for output, tokens in self.get_streaming_response(prompt, openapi_url, openapi_session_id, openapi_request_output_len):
+            yield output

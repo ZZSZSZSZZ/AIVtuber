3cf436b719b5e19ee9f7620fb53e6de80f426b77
ZZSZSZSZZ <13023184453@163.com>
2023-11-28 23:38:24 +0800

    优化了配置文件管理器

--- tools/config_manager.py
+++ tools/config_manager.py
@@ -4,16 +4,16 @@ import yaml
 class Config:
     json = None  # json格式的配置文件
 
-    def load_config(self, yamlfile=None):
+    def load_config(self, yaml_file=None):
         """
         用于加载配置文件
-        :param yamlfile: yaml文件路径
+        :param yaml_file: yaml文件路径
         :return: json格式的配置文件
         """
-        if yamlfile is None:
+        if yaml_file is None:
             print("Config文件未指定:(")
         try:
-            file = open(yamlfile, 'r', encoding='utf-8')
+            file = open(yaml_file, 'r', encoding='utf-8')
             self.json = yaml.load(file, Loader=yaml.FullLoader)
         except IOError as e:
             print(e)

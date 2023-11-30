cafd4ebe8b22360b943b1b0b1f90d5973e3403b6
ZZSZSZSZZ <13023184453@163.com>
2023-11-30 22:23:54 +0800

    更新

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from models import OpenAI
 from tools import Config
-from document import TestLoader
 
 config = Config().load_config('config.yml')
 
-print(OpenAI().get_message("你需要扮演一个判断高手，我将会给你一个弹幕，如果符合回答要求则输出true，否则输出false。回答要求：要判断有没有回答性，禁止回答带有攻击性的问题，禁止回答带有政治的问题。弹幕：", ""))
+# print(OpenAI().get_message("你是谁", "你需要扮演一个判断高手，我将会给你一个弹幕，如果符合回答要求则输出true，否则输出false。回答要求：要判断有没有回答性，禁止回答带有攻击性的问题，禁止回答带有政治的问题。"))
+
+content = "你觉得我可爱吗"
+
+if OpenAI().get_message(content, "你需要扮演一个判断高手，我将会给你一个弹幕，如果符合回答要求输出true，否则输出false。回答要求：要判断有没有回答性，禁止回答带有攻击性的问题，禁止回答带有政治的问题。") == "true":
+    print(OpenAI().get_message(content))
 
 # print(OpenAI().get_embeddings("你是谁"))
 

0cd1c8694554a294a7d22c5308cabff9106ba92a
ZZSZSZSZZ <13023184453@163.com>
2023-11-30 22:54:03 +0800

    更新

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ content = "你觉得我可爱吗"
 
 if OpenAI().get_message(content, "你需要扮演一个判断高手，我将会给你一个弹幕，如果符合回答要求输出true，否则输出false。回答要求：要判断有没有回答性，禁止回答带有攻击性的问题，禁止回答带有政治的问题。") == "true":
     print(OpenAI().get_message(content))
+else:
+    print("false")
 
 # print(OpenAI().get_embeddings("你是谁"))
 
